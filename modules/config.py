e5558769d1ef3814bb550d9b6daaab1a283c9450
rsrawatofficial <rsrasasingh@gmail.com>
2025-05-07 16:49:45 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,7 +1,7 @@
 # config.py
 import os
 BOT_NAME = os.environ.get('BOT_NAME',)
-BOT_TOKEN = os.environ.get('BOT_TOKEN',  "8003649544:AAGoiThVN8KLJyKGsGf1BcfTsjDTrSmjFR8")
+BOT_TOKEN = os.environ.get('BOT_TOKEN',  "7609528498:AAGqcl_h-Cygc4v_oiXqD4cbp-yGypVcakY")
 API_ID = int(os.environ.get('API_ID',  "27900743")
 API_HASH = os.environ.get('API_HASH', "ebb06ea8d41420e60b29140dcee902fc")
 MONGO_URI = os.environ.get('MONGO_URI', "mongodb+srv://rsrasasingh:FS2G9YbI28KbPHLC@cluster0.yljr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
